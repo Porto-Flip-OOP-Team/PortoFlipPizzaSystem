f569e0356ba6dc8eb0ea3c703b8796f6c22cc80f
dushka-dragoeva <d.dragoeva@abv.bg>
2017-01-09 20:56:08 +0200

    Work over project structure

---
+++ PortoFlipPizzaSystem/ProtoFlipPizzaSystem.ConsoleClient/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PortoFlipPizzaSystem.Core;
+
+namespace ProtoFlipPizzaSystem.ConsoleClient
+{
+    class Startup
+    {
+        static void Main()
+        {
+            var app = new App();
+            app.RunProgram();
+        }
+    }
+}
