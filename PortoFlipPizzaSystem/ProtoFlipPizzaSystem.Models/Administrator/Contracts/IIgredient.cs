499d7c268b23cf7e8311f6c55c0abb30225e8738
dushka-dragoeva <d.dragoeva@abv.bg>
2017-01-10 10:46:13 +0200

    Added Properties and methods in IIngredients

--- PortoFlipPizzaSystem/ProtoFlipPizzaSystem.Models/Administrator/Contracts/IIgredient.cs
+++ PortoFlipPizzaSystem/ProtoFlipPizzaSystem.Models/Administrator/Contracts/IIgredient.cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using ProtoFlipPizzaSystem.Models.Enums;
+
 namespace ProtoFlipPizzaSystem.Models.Administrator.Contacts
 {
     interface IIgredient
     {
+        string Name { get; }
 
+        UnitType UnitType { get; }
+
+        decimal CalculateUnitPrice();
+
+        decimal TotalPrice { get; }
+
+        decimal Quantity { get; }
     }
 }
