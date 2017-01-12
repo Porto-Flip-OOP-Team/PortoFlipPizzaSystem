4704ecf9314c7f77bee4db2cd0b686e3a4b072a5
KaloyanchoSt <KaloyanchoSt@gmail.com>
2017-01-12 19:31:18 +0200

    Added Validator

--- PortoFlipPizzaSystem/ProtoFlipPizzaSystem.ConsoleClient/Startup.cs
+++ PortoFlipPizzaSystem/ProtoFlipPizzaSystem.ConsoleClient/Startup.cs
@@ -1,22 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using PortoFlipPizzaSystem.Core;
-
+﻿using PortoFlipPizzaSystem.Core;
 
 namespace ProtoFlipPizzaSystem.ConsoleClient
 {
-    class Startup
+    public class Startup
     {
-        static void Main()
+        public static void Main()
         {
-
             var app = new App();
             app.RunProgram();
-
         }
     }
 }
---
+++ PortoFlipPizzaSystem/ProtoFlipPizzaSystem.Models/Validation/Validator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ProtoFlipPizzaSystem.Models.Validation
+{
+    public static class Validator
+    {
+        /// <summary>
+        /// Checks if the given integer number is between the required range
+        /// </summary>
+        /// <param name="value">integer number</param>
+        /// <param name="min">the minimal range</param>
+        /// <param name="max">the maxixmum range</param>
+        /// <param name="message">the exception message</param>
+        public static void ValidateIntRange(int value, int min, int max, string message)
+        {
+            if (value < min || value > max)
+            {
+                throw new ArgumentException(message);
+            }
+        }
+
+        /// <summary>
+        /// Checks if given decimal number is between the required range
+        /// </summary>
+        /// <param name="value">value as decimal number</param>
+        /// <param name="min">the minimal range</param>
+        /// <param name="max">the maxixmum range</param>
+        /// <param name="message">the exception message</param>
+        public static void ValidateDecimalRange(decimal value, decimal min, decimal max, string message)
+        {
+            if (value < min || value > max)
+            {
+                throw new ArgumentException(message);
+            }
+        }
+
+        /// <summary>
+        /// Checks if the given object is null or not
+        /// </summary>
+        /// <param name="value">the object</param>
+        /// <param name="message">the exception message</param>
+        public static void ValidateNull(object value, string message)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(message);
+            }
+        }
+
+        /// <summary>
+        /// Checks if the symbols are valid for different verifications
+        /// </summary>
+        /// <param name="value">value as string</param>
+        /// <param name="pattern">pattern for validation</param>
+        /// <param name="message">the exception message</param>
+        public static void ValidateSymbols(string value, string pattern, string message)
+        {
+            var regex = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            if (!regex.IsMatch(value))
+            {
+                throw new ArgumentException(message);
+            }
+        }
+    }
+}
